5b90a2bffd584098883fb6b5a28cb704d004f1c6
Dominik <dominik.switlik@gmail.com>
2023-12-20 12:01:41 +0100

    project init

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+
+    <?php
+        $servername = "localhost";
+        $username = "praktykidominik_base";
+        $password = "praktykidominik";
+
+        try {
+            $conn = new PDO("mysql:host=$servername;dbname=praktykidominik_base", $username, $password);
+
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connected successfully";
+        } catch(PDOException $e) {
+            echo "Connection failed: " . $e->getMessage();
+        }
+
+
+    ?>
+
+
+
+</body>
+</html>
